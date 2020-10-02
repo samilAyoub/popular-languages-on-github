ce08ea9a48831792b711eafa4a6d6b93c9d92a67
Samil Ayoub <ssamil.ayoub@gmail.com>
2020-10-02 19:18:08 +0100

    Modify micro service
    Modify the microservice to list the languages used by the 100
    trending public repos.

--- Consume_github_api.py
+++ Consume_github_api.py
@@ -1,20 +1,31 @@
-
-from flask import Flask, request
 from datetime import date, timedelta
-
+from collections import defaultdict
 import requests
+from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def list_trending_repos():
+def list_trending_lang():
     response = requests.get(
         url="https://api.github.com/search/repositories?q=created:>"
         + str(date.today() - timedelta(days=30))
-        + "&sort=stars&order=desc"
+        + "&sort=stars&order=desc&per_page=100&page=1"
     )
-    return response.json()
+
+    repos = response.json().get('items')
+    d = {}
+    for repo in repos:
+        if repo['language'] != None:
+            lang = repo['language']
+            repo_name = repo['name']
+            d[lang] = d.get(lang, {'nbr': 0,
+                                   'repos': []})
+            d[lang].update({'nbr': d[lang]['nbr'] + 1,
+                            'repos': d[lang]['repos'] + [repo_name]})
+
+    return make_response(d, 200)
 
 
 if __name__ == "__main__":
